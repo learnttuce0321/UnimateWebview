8fdf7b581e8f4b75a196c2109b4f1976b31dd6aa
SMJ426 <jerry04260@gmail.com>
2025-04-01 00:08:20 +0900

    style(register-product): 제목 입력 Form 스타일링

--- src/app/register/_components/registerForm/RegisterInput.tsx
+++ src/app/register/_components/registerForm/RegisterInput.tsx
@@ -19,11 +19,19 @@ export default function RegisterInput({
   error,
   errorMessage = '입력이 필요합니다.',
 }: Props) {
+  // TODO : 우선은 제목 form만 했음, 다만 다른 입력 폼들도 props로 구분해서 중복 로직 없도록 할 예정
   return (
-    <div className="mb-4">
-      <div className="border-[1px] border-solid p-2 rounded">
+    <div className="mb-4 mt-[30px] flex flex-col gap-[16px] justify-start">
+      <label
+        htmlFor="register-product-title"
+        className="text-gray-900 font-bold text-[14px]"
+      >
+        제목
+      </label>
+      <div className="h-[50px] border-[1px] bg-white border-gray-200 border-solid py-[14px] px-[16px] rounded ">
         <input
-          className="w-full outline-none"
+          className="w-full outline-none font-[16px] "
+          id="register-product-title"
           type={type}
           placeholder={placeholder}
           {...register(name, { required })}
