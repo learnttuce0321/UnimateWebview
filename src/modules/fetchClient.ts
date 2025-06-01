daa3dd28a52e1824d29041b67164d088f3e1daad
JuSanghoo <lettucegood2@gmail.com>
2025-06-01 15:50:46 +0900

    feat(fetch-client): 요청 방식 추가,타입 지정, 에러 response 리턴 방식 추가

--- src/modules/fetchClient.ts
+++ src/modules/fetchClient.ts
@@ -1,17 +1,74 @@
 import { processEnvBaseUrl } from '@/constant/environments';
 
+export interface ApiRequest {
+  url: string;
+  params?: Record<string, any>;
+  body?: Record<string, any>;
+}
 export interface ApiResponse {
   status: number;
   code: string;
 }
 
-export type ApiResponseError = ApiResponse & {
+export interface ApiResponseError extends ApiResponse {
   message: string;
   isError: boolean;
-};
+}
 
-export const fetchClient = async <TResponse>(
-  url: string,
+const fetchClient = () => {
+  const GET = async <TResponse = unknown>(
+    { url, params }: ApiRequest,
+    init: Omit<RequestInit, 'credentials' | 'method'> = {}
+  ) => {
+    return await request<TResponse>(
+      { url, params },
+      { method: 'GET', ...init }
+    );
+  };
+
+  const POST = async <TResponse = unknown>(
+    { url, params, body }: ApiRequest,
+    init: Omit<RequestInit, 'credentials'> = {}
+  ) => {
+    return await request<TResponse>(
+      { url, params, body },
+      { method: 'POST', ...init }
+    );
+  };
+
+  const PUT = async <TResponse = unknown>(
+    { url, params, body }: ApiRequest,
+    init: Omit<RequestInit, 'credentials'> = {}
+  ) => {
+    return await request<TResponse>(
+      { url, params, body },
+      { method: 'PUT', ...init }
+    );
+  };
+  const DELETE = async <TResponse = unknown>(
+    { url, params, body }: ApiRequest,
+    init: Omit<RequestInit, 'credentials'> = {}
+  ) => {
+    return await request<TResponse>(
+      { url, params, body },
+      { method: 'DELETE', ...init }
+    );
+  };
+
+  return {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+  };
+};
+export default fetchClient();
+
+/**
+ * @description Fetch API를 사용하여 HTTP 요청을 보내는 함수입니다. 호출하는 위치에 따라 에러 리턴이 다릅니다.
+ */
+const request = async <TResponse>(
+  { url, params, body }: ApiRequest,
   init: Omit<RequestInit, 'credentials'> = {}
 ) => {
   // TODO: vanilla/zustand를 통해 accessToken 가져오기
@@ -22,27 +79,56 @@ export const fetchClient = async <TResponse>(
   const headers = new Headers(init.headers);
   if (token) headers.set('Authorization', `Bearer ${token}`);
 
-  console.log('processEnvBaseUrl', processEnvBaseUrl);
-  console.log('fetchClient url', url);
-  const response = await fetch(`${processEnvBaseUrl}${url}`, {
-    ...init,
-    headers,
-    cache: 'no-store',
-  });
+  try {
+    const response = await fetch(
+      `${processEnvBaseUrl}${generateURLWithParams({ url: url, params: params })}`,
+      {
+        ...init,
+        ...(body && { body: JSON.stringify(body) }),
+        headers,
+        cache: 'no-store',
+      }
+    );
 
-  if (!response.ok) {
-    console.log('[ERROR]', response);
+    if (!response.ok) {
+      throw response;
+    }
+
+    const data: TResponse = await response.json();
+    return data;
+  } catch (error: any) {
+    console.log('[ERROR]', error);
 
     const errorResponse: ApiResponseError = {
-      status: response.status,
-      code: response.statusText,
-      message: response.statusText,
+      status: error.status,
+      code: error.statusText,
+      message: error.statusText,
       isError: true,
     };
 
-    return errorResponse;
+    const isServer = typeof window === 'undefined';
+
+    if (isServer) {
+      return errorResponse;
+    } else {
+      throw errorResponse;
+    }
+  }
+};
+
+type GeneratedUrlWithParams = {
+  url: string;
+  params?: Record<string, any>;
+};
+
+export const generateURLWithParams = ({
+  url,
+  params,
+}: GeneratedUrlWithParams) => {
+  if (params && Object.keys(params).length > 0) {
+    const searchParams = new URLSearchParams(params);
+    return `${url}?${searchParams.toString()}`;
   }
 
-  const data: TResponse = await response.json();
-  return data;
+  return url;
 };
