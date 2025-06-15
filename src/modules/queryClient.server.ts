e9c76ada67fc4e9fb2bf34a2d25372de276ebf24
SMJ426 <jerry04260@gmail.com>
2025-06-15 16:31:29 +0900

    feat: api client 로직 추가

---
+++ src/modules/queryClient.server.ts
@@ -0,0 +1,35 @@
+import { QueryClient } from '@tanstack/react-query';
+import fetchClient from './fetchClient';
+
+interface FetchQueryParams {
+  url: string;
+  params?: Record<string, any>;
+}
+
+export const createSSRQueryClient = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchIntervalInBackground: false,
+        gcTime: 1000 * 60 * 60,
+      },
+    },
+  });
+
+  return {
+    queryClient,
+
+    fetchQuery: async <T = unknown>({
+      url,
+      params,
+    }: FetchQueryParams): Promise<T> => {
+      return await queryClient.fetchQuery({
+        queryKey: [url, params],
+        queryFn: () => fetchClient.GET<T>({ url, params }),
+      });
+    },
+  };
+};
