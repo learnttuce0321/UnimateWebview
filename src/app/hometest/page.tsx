7746015cf714e6dc2ea8148c60f2c45b71549b51
SMJ426 <jerry04260@gmail.com>
2025-07-16 23:24:21 +0900

    fix: 빌드 에러 수정

--- src/app/hometest/page.tsx
+++ src/app/hometest/page.tsx
@@ -4,6 +4,9 @@ import { API_PRODUCTS_LIST } from 'modules/keyFactory';
 import { createSSRQueryClient } from 'modules/queryClient.server';
 import ProductPostList from './_components/ProductPostsList';
 
+// fetchClient.ts 의 cache:'no-store' 옵션 때문에 빌드 에러가 발생해서 임시로 동적으로 페이지 생성
+export const dynamic = 'force-dynamic';
+
 export default async function TestHomePage() {
   const { queryClient, fetchQuery } = createSSRQueryClient();
 
