59c18b98b9ee3e4bda8bf3fd478b5a304f65fe61
SMJ426 <jerry04260@gmail.com>
2025-07-20 14:22:03 +0900

    test: 레포 test push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ const Page = ({ searchParams: { cityId } }: Props) => {
   console.log('here');
   return (
     <>
+    <span>테스트 푸시입니다. 롤벡 예정 - 민재 </span>
       <HomeHeader />
       <ProductList cityId={normalizeString(cityId)} />
     </>
