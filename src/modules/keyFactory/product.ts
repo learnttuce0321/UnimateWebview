0a0ca8da5cae2455d54291d0fda46d8354fbf06f
Front-Sanghoo <lettucegood2@gmail.com>
2025-10-08 17:36:03 +0900

    feat: 에러 타입 수정 및 mutation 훅 분리

--- src/modules/keyFactory/product.ts
+++ src/modules/keyFactory/product.ts
@@ -19,15 +19,33 @@ export const API_PRODUCTS_SEARCH = '/api/v1/product-posts/search';
 /**
  * @POST 상품 이미지 업로드를 위한 Presigned URL 발급 API
  */
-export const API_PRODUCTS_POSTS_PRESIGNED_URL =
+export const API_PRODUCT_POSTS_PRESIGNED_URL =
   '/api/v1/product-posts/presigned-url';
 
 /**
  * @GET 내가 찜한 상품게시글 리스트 API
  */
-export const API_PRODUCTS_LIKE = '/api/v1/product-posts/my/likes';
+export const API_MY_LIKE_PRODUCTS = '/api/v1/product-posts/my/likes';
 
 /**
  * @GET 나의 판매내역 목록 조회 API
  */
 export const API_MY_SALES_PRODUCTS = '/api/v1/product-posts/my/sales';
+
+/**
+ * @PATCH 상품 게시글 숨기기 API
+ */
+export const API_PRODUCT_HIDE = (productId: number) =>
+  `/api/v1/product-posts/${productId}/hide`;
+
+/**
+ * @PATCH 상품 게시글 숨김 해제 API
+ */
+export const API_PRODUCT_UNHIDE = (productId: number) =>
+  `/api/v1/product-posts/${productId}/unhide`;
+
+/**
+ * @DELETE 상품 게시글 삭제 API
+ */
+export const API_PRODUCT_DELETE = (productId: number) =>
+  `/api/v1/product-posts/${productId}`;
