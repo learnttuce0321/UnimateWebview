11ac2e22de7ec95f15dd94ff7797191484bb85db
Front-Sanghoo <lettucegood2@gmail.com>
2025-10-11 21:56:15 +0900

    Revert "test"
    This reverts commit d11dcb1f2faa6bf85ffaad5a2a9766f48ed2bd14.

--- src/components/LoginWrapper.tsx
+++ src/components/LoginWrapper.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useAppStore } from 'providers/ZustandProvider';
-import deviceInfoStore from 'stores/vanillaStore.deviceInfo';
 
 interface Props {
   children: React.ReactNode;
@@ -9,15 +8,9 @@ interface Props {
 
 const LoginWrapper = ({ children }: Props) => {
   const isLogin = useAppStore((state) => state.isLogin);
-  const accessToken = deviceInfoStore.getState().deviceInfo.accessToken;
 
   if (!isLogin) {
-    return (
-      <>
-        <p className="h-[100px] w-full break-all">{accessToken}</p>{' '}
-        {'잘못된 접근입니다.'}
-      </>
-    );
+    return '잘못된 접근입니다.';
   }
 
   return <>{children}</>;
