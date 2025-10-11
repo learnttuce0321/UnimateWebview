03f84766c3c889bc72fdfbf306910e9620560eae
Front-Sanghoo <lettucegood2@gmail.com>
2025-10-11 19:23:08 +0900

    fix: build에러 수정

--- src/app/search/result/_components/searchedProduct/SearchedProductCard.tsx
+++ src/app/search/result/_components/searchedProduct/SearchedProductCard.tsx
@@ -1,5 +1,5 @@
-import ProductInfo from 'app/_components/product/ProductInfo';
-import ProductThumbnail from 'app/_components/product/ProductThumbnail';
+import LocalizedProductInfo from 'app/_components/product/LocalizedProductInfo';
+import LocalizedProductThumbnail from 'app/_components/product/LocalizedProductThumbnail';
 
 interface Props {
   product: any;
@@ -9,8 +9,8 @@ const SearchedProductCard = ({ product }: Props) => {
   return (
     <a href={`/product/${product.id}`}>
       <div className="mb-[8px] flex h-[140px] justify-between gap-[12px] rounded-[8px] bg-white p-[16px] shadow-[0px_0px_10px_rgba(0,0,0,0.05)]">
-        <ProductThumbnail thumbnailUrl={product.imageUrl} />
-        <ProductInfo {...product} />
+        <LocalizedProductThumbnail thumbnailUrl={product.imageUrl} />
+        <LocalizedProductInfo {...product} />
       </div>
     </a>
   );
