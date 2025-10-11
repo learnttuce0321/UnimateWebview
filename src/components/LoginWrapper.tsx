d11dcb1f2faa6bf85ffaad5a2a9766f48ed2bd14
Front-Sanghoo <lettucegood2@gmail.com>
2025-10-11 19:16:06 +0900

    test

--- src/components/LoginWrapper.tsx
+++ src/components/LoginWrapper.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useAppStore } from 'providers/ZustandProvider';
+import deviceInfoStore from 'stores/vanillaStore.deviceInfo';
 
 interface Props {
   children: React.ReactNode;
@@ -8,9 +9,14 @@ interface Props {
 
 const LoginWrapper = ({ children }: Props) => {
   const isLogin = useAppStore((state) => state.isLogin);
+  const accessToken = deviceInfoStore.getState().deviceInfo.accessToken;
 
   if (!isLogin) {
-    return '잘못된 접근입니다.';
+    return (
+      <>
+        <p className="h-[100px] w-full break-all">{accessToken}</p> {'잘못된 접근입니다.'}
+      </>
+    );
   }
 
   return <>{children}</>;
