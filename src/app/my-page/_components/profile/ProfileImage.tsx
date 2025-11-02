fc4af2a458c006703ae48c5f666174b64cb5d66f
Front-Sanghoo <lettucegood2@gmail.com>
2025-11-02 19:16:22 +0900

    fix: 프로필 이미지 크기 수정

--- src/app/my-page/_components/profile/ProfileImage.tsx
+++ src/app/my-page/_components/profile/ProfileImage.tsx
@@ -9,7 +9,13 @@ const ProfileImage = () => {
 
   return (
     <div className="h-[96px] w-[96px] rounded-full bg-gray-400">
-      <img src={profileImageUrl} width={96} height={96} alt="프로필 이미지" />
+      <img
+        className="h-full w-full rounded-full object-cover"
+        src={profileImageUrl}
+        width={96}
+        height={96}
+        alt="프로필 이미지"
+      />
     </div>
   );
 };
