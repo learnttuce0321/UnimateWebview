4f984c86a0a6a8b502226c752bd8b0bd179586b2
Front-Sanghoo <lettucegood2@gmail.com>
2025-11-02 19:14:49 +0900

    ㅅㄷㄴㅅ

--- src/app/my-page/_components/MyPageListItem.tsx
+++ src/app/my-page/_components/MyPageListItem.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import navigationScheme from 'utils/navigationScheme';
 
 interface Props {
@@ -9,15 +10,17 @@ interface Props {
 
 const MyPageListItem = ({ title, targetUrl }: Props) => {
   const { openWeb } = navigationScheme();
+  const [state, setState] = useState(false);
 
   const handleClick = () => {
     openWeb(targetUrl);
+    setState(true);
   };
 
   return (
     <div
       onClick={handleClick}
-      className="flex h-[56px] w-full items-center justify-between"
+      className={`flex h-[56px] w-full items-center justify-between ${state ? 'bg-red' : ''}`}
     >
       <span className="text-[14px] leading-[14px] text-blue_gray-800">
         {title}
