c02ddfadc0879af5f8e10e9cd5e599338c6438b7
Front-Sanghoo <lettucegood2@gmail.com>
2025-11-08 14:36:17 +0900

    fix: 관심 도시 설정 시, 주코드가 보이도록 수정

--- src/app/interest/_components/search/SearchedCity.tsx
+++ src/app/interest/_components/search/SearchedCity.tsx
@@ -9,12 +9,13 @@ interface Props {
 }
 
 const SearchedCity = ({ region, isSelectedRegion, onClick }: Props) => {
+  const { name: regionName, admin1Code: regionAdminCode } = region;
   return (
     <li
       className={`line-clamp-1 flex h-[45px] w-full items-center border-[1px] pl-[16px] text-[16px] ${isSelectedRegion ? 'rounded-[10px] border-blue-600_P bg-blue_gray-50 text-blue-600_P' : 'border-white'}`}
       onClick={() => onClick(region)}
     >
-      {region.name}
+      {`${regionName}, ${regionAdminCode}`}
     </li>
   );
 };
